edeaad2e8db58b45c904f039045f5805911a514d
Adam Naber <casual.formality@gmail.com>
2016-01-28 13:41:42 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This project is inspired by Hatnote's [Listen to Wikipedia(https://github.com/hatnote/listen-to-wikipedia) Project for people without consistent internet connections.
+
+## About
+
+Dynamic, pseudorandom background noise generation
+
+## Sound
+
+The sound included here were copied from the Listen to Wikipedia project
---
+++ sounds.py
@@ -0,0 +1,88 @@
+#!/usr/local/bin/python
+
+# 
+# This version uses pygame to play the sounds (more efficient)
+# 
+
+import sys, pygame, _thread, time, random, os, threading, scipy
+import pygame.mixer
+
+from os import listdir
+from os.path import isfile, join
+
+def block_and_play(sounds, sem):
+    while 1:
+        sem.acquire()
+        sound = random.choice(sounds)
+        sound.play()
+
+def play_sounds(dirname, freq):
+    my_volume = volume
+    scipy.random.seed()
+
+    files = listdir(dirname)
+    # Get full path for files
+    files = [f for f in files if isfile(join(dirname, f))]
+    # Filter out everything that isn't an OGG
+    files = [f for f in files if f.endswith(".ogg")]
+
+    # Preload sound list.  Otherwise, this becomes a total
+    #  resource hog.  If this takes up too much RAM, consider
+    #  reducing the number of sounds before changing this
+    #  behavior.  It *really* takes a lot of resources to
+    #  constantly load and unload the sound files.
+    sounds = []
+    for f in files:
+        sound = pygame.mixer.Sound(join(dirname, f))
+        # Set volume for each sound to sane level
+        sound.set_volume(volume)
+        sounds.append(sound)
+
+    # This function signals to any of the listening threads to
+    #  start playing a random sound whenever this semaphore is
+    #  released (incremented).  The first thread to catch it
+    #  acquires (decrements) the semaphore, so no other thread
+    #  sees it.
+    sem = threading.Semaphore()
+
+    # Since the sounds aren't particularly long, creating freq/5
+    #  listener threads seems like a sane value. If you find the
+    #  threads are blocking needlessly long to play the sound, or
+    #  if this number hogs too many resources, change as needed.
+    processes = []
+    for i in range(int(freq/5)):
+        _thread.start_new_thread(block_and_play, (sounds, sem, ))
+
+    # Wait a random amount of time between 1 and 100 seconds before
+    #  telling the listeners to play a random sound from its list
+    while 1:
+        sleep_time = scipy.random.random_integers(1, 100)
+        time.sleep(sleep_time / freq)
+        sem.release()
+
+# Initialize modules
+pygame.init()
+pygame.mixer.init()
+
+if(len(sys.argv) > 1):
+    try:
+        volume = float(sys.argv[1]);
+    except ValueError:
+        print("Usage: ./sounds.py [volume]")
+        sys.exit()
+else:
+    volume = 0.2;
+
+try:
+    # Start a randomizer thread for each sound type
+    _thread.start_new_thread(play_sounds, ('clav/', 100.0, ) )
+    _thread.start_new_thread(play_sounds, ('celesta/', 100.0, ) )
+    _thread.start_new_thread(play_sounds, ('swells/', 5.0, ) )
+except:
+    print("Error: unable to start thread")
+
+# Wait for user input to quit nicely
+while True:
+    input("Press [Enter] to quit.")
+    pygame.quit()
+    sys.exit()
